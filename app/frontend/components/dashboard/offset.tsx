d502ecaf1f4566ec703883f299c87e5971df7b23
zlaud <laudkimberly@gmail.com>
2025-10-26 07:21:57 -0700

    offset page

--- app/frontend/components/dashboard/offset.tsx
+++ app/frontend/components/dashboard/offset.tsx
@@ -1,12 +1,13 @@
 import { Sprout } from "lucide-react";
 import { Button } from "../ui/button";
+import Link from "next/link";
 const Offset = () => {
   return (
-    <div>
+    <Link href="/offset">
       <Button className="flex items-center gap-2 cursor-pointer bg-[#4a6b8a]">
         <Sprout /> Offset
       </Button>
-    </div>
+    </Link>
   );
 };
 
