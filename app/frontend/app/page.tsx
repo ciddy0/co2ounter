dc8b58ebc4e8fd9fb04d7fb8c18888c464d7ca29
smngo100 <smngo100@gmail.com>
2025-10-26 09:45:33 -0700

    FIX: install button rounded corners

--- app/frontend/app/page.tsx
+++ app/frontend/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
             </h2>
             <a
               href="#"
-              className="px-8 py-3 bg-green-600 text-white font-semibold rounded hover:bg-green-700 transition"
+              className="px-8 py-3 bg-green-600 text-white font-semibold rounded-lg hover:bg-green-700 transition"
             >
               Install to Google Chrome
             </a>
