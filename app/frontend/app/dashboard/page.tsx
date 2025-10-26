af79abfddee74fe96e85957101b6b7f5ef408772
zlaud <laudkimberly@gmail.com>
2025-10-25 23:30:39 -0700

    weekly bar chart

--- app/frontend/app/dashboard/page.tsx
+++ app/frontend/app/dashboard/page.tsx
@@ -1,9 +1,23 @@
 import Heatmap from "../../components/dashboard/heatmap";
+import PromptCard from "../../components/dashboard/promptcard";
+import DailyCards from "../../components/dashboard/cards";
+import CO2Card from "../../components/dashboard/co2card";
+import WeeklyChart from "../../components/dashboard/weeklychart";
+import databaseValues from "../../data.js";
 
 export default function Dashboard() {
   return (
-    <div className="p-6">
+    <div className="p-6 min-w-full">
+      <div className="w-full grid grid-cols-4 gap-4">
+        <DailyCards number={10} title="Today's Prompts" increment={5} />
+        <PromptCard number={130} title="Lifetime Prompts" increment={40} />
+        <DailyCards number={10} title="Today's CO2 Emissions" increment={-5} />
+        <CO2Card number={676} title="Lifetime CO2 Emissions" increment={10} />
+      </div>
       <Heatmap />
+      <div className="w-2/3">
+        <WeeklyChart title="Weekly CO2 Emissions" data={databaseValues} />
+      </div>
     </div>
   );
 }
---
+++ app/frontend/components/dashboard/cards.tsx
@@ -0,0 +1,40 @@
+import { ArrowDownRight, ArrowUpRight } from "lucide-react";
+
+const DailyCards = ({
+  number,
+  title,
+  increment,
+}: {
+  number: number;
+  title: string;
+  increment: number;
+}) => {
+  return (
+    <div className="flex flex-col gap-4 p-6 bg-white rounded-lg shadow-md">
+      <h2 className="font-semibold">{title}</h2>
+      <h1 className="text-3xl font-bold">{number}</h1>
+
+      <div className="flex gap-2 items-center">
+        {increment > 0 ? (
+          <>
+            <div className="flex gap-1 items-center border border-green-500 rounded-lg px-2">
+              <h3 className="text-sm font-semibold">{increment}</h3>
+              <ArrowUpRight className="text-green-500 w-4 h-4" />
+            </div>
+            <span className="text-sm ">increase from yesterday</span>
+          </>
+        ) : (
+          <>
+            <div className="flex gap-1 items-center border border-red-500 rounded-lg px-2">
+              <h3 className="text-sm font-semibold">{Math.abs(increment)}</h3>
+              <ArrowDownRight className="text-red-500 w-4 h-4" />
+            </div>
+            <span className="text-sm">decrease from yesterday</span>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default DailyCards;
