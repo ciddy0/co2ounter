ea41be4c600c1c2196b27b5071f00db7134e14cd
smngo100 <smngo100@gmail.com>
2025-10-25 20:33:54 -0700

    FIX: light and dark mode

--- app/frontend/app/login/page.js
+++ app/frontend/app/login/page.js
@@ -7,7 +7,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import {
   Card,
-  // CardAction,
   CardContent,
   CardDescription,
   CardFooter,
@@ -67,7 +66,7 @@ export default function LoginPage() {
 
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <Card className="w-1/2 max-w-md gap-4 border-[0.5px] border-gray-500">
+      <Card className="w-1/2 max-w-md gap-4 border border-border">
         <CardHeader>
           <CardTitle>Login to your account</CardTitle>
           <CardDescription>Enter your information to sign in</CardDescription>
@@ -80,7 +79,7 @@ export default function LoginPage() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder=""
-                className="w-full h-10 p-3 bg-[#212121] border-[0.5px] border-gray-500 rounded-md "
+                className="w-full h-10 p-3 bg-input border border-border rounded-md "
               />
             </div>
             <div className="space-y-2">
@@ -90,12 +89,12 @@ export default function LoginPage() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder=""
-                className="w-full h-10 p-3 bg-[#212121] border-[0.5px] border-gray-500 rounded-md "
+                className="w-full h-10 p-3 bg-input border border-border rounded-md "
               />
             </div>
             <button
               type="submit"
-              className="mt-2 w-full h-10 bg-white text-black rounded-md p-1"
+              className="mt-2 w-full h-10 bg-primary text-primary-foreground rounded-md p-1"
             >
               Login
             </button>
